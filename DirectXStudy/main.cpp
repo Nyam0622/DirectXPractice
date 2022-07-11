464f082734b915a83764bcac35612d0565b5c35b
Jeon <dbs_dmswl92@naver.com>
2022-07-11 21:38:35 +0900

    SimpleWindow
    확인용 간단한 메시지 창 출력

---
+++ DirectXStudy/main.cpp
@@ -0,0 +1,11 @@
+#include<Windows.h>
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+	MessageBox(NULL, L"Hello World", L"SimpleWindow", MB_ICONEXCLAMATION | MB_OK);
+	return 0;
+}
